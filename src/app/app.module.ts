495feb7aca782a3c84d1aef04cb519925f854876
oaymohsin <oaymohsin@gmail.com>
2024-03-29 01:23:13 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './core/banner/banner.component';
+import { HeaderComponent } from './core/header/header.component';
+
+import { BrowseComponent } from './pages/browse/browse.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    LoginComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
